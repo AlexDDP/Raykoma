30742da7bee57d90160df071dd177db2af97f623
Aryan Dhiman <aryanoisin.dhiman@gmail.com>
2025-03-02 23:20:59 +0000

    Score System
    Got the score to deecrease with rock collision but not for dodging rocks

--- Assets/BoatCollision.cs
+++ Assets/BoatCollision.cs
@@ -5,8 +5,14 @@ using TMPro;
 public class BoatCollision : MonoBehaviour
 {
     private Rigidbody2D rb;
-    private int collisionCounter = 0;
+    public int collisionCounter = 0;
     public TMP_Text collisionText;
+
+    public int PlayerScore = 0;
+
+    public TMP_Text Score;
+
+
 
     void Start()
     {
@@ -19,19 +25,29 @@ public class BoatCollision : MonoBehaviour
 
     void OnCollisionEnter2D(Collision2D collision)  // FIX -- -- Use OnCollisionEnter2D for 2D physics
     {
-        if (collision.gameObject.CompareTag("Rock"))  
+        if (collision.gameObject.CompareTag("Rock") == true)  
         {
-            collisionCounter++;
 
+            collisionCounter++;
+            PlayerScore = PlayerScore - 20;
+            Debug.Log("Score is down by 20!");
+            
+        }
             if (collisionText != null)
             {
                 collisionText.text = "Collisions: " + collisionCounter;
+            }
+            if(Score != null) 
+            {
+                Score.text = "Score: " + PlayerScore;
             }
 
             //DEBUG LOG
             Debug.Log("Collision detected! Total collisions: " + collisionCounter);
 
             rb.angularVelocity = 0f;
-        }
-    }
+        }    
+    
+    
 }
+
