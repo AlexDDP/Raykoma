66e89e3d1d2c0961d6b2288459620efef3839f11
oisinmaher <62963896+oisinmaher@users.noreply.github.com>
2025-03-06 21:34:52 +0000

    Update waterDrag.cs

--- Assets/waterDrag.cs
+++ Assets/waterDrag.cs
@@ -5,16 +5,23 @@ public class waterDrag : MonoBehaviour
     public Rigidbody2D drag;
     public float timeTilExpand = 0.3f;
     public float timer;
-    public float pivotPos;
-    public float prevPivot;
+    public float pivotPosX;
+    public float pivotPosY;
+    public float prevPivotX;
+    public float prevPivotY;
     public float spriteWidth;
-    public float scaleIncrease = 0.25f;
+    public float spriteHeigth;
+    public float scaleIncreaseX = 0.25f;
+    public float scaleIncreaseY = 0.075f;
     private void Start()
     {
         timer = timeTilExpand;
         spriteWidth = GetComponent<SpriteRenderer>().bounds.size.x;
-        pivotPos = -1.22f - (spriteWidth / 2);
-        prevPivot = pivotPos;
+        spriteHeigth = GetComponent<SpriteRenderer>().bounds.size.y;
+        pivotPosX = -spriteWidth / 2; ;
+        prevPivotX = pivotPosX;
+        pivotPosY = -spriteHeigth / 2; ;
+        prevPivotY = pivotPosY;
         drag = GetComponent<Rigidbody2D>(); 
     }
     private void Update()
@@ -22,15 +29,18 @@ public class waterDrag : MonoBehaviour
         if(timer <= 0)
         {
             // makes drag longer
-            transform.localScale += new Vector3(scaleIncrease, 0f, 0f);
+            transform.localScale += new Vector3(scaleIncreaseX, scaleIncreaseY, 0f);
             // finds sprite width after being scaled
             spriteWidth = GetComponent<SpriteRenderer>().bounds.size.x;
+            spriteHeigth = GetComponent<SpriteRenderer>().bounds.size.y;
             // calculation for where new pivot point should be (left side of kayak - half the width of sprite);
-            pivotPos = -1.22f - (spriteWidth / 2);
+            pivotPosX = -spriteWidth / 2;
+            pivotPosY = -spriteHeigth / 2;
             // incrementing its position based on the difference of current and previous pivot positon
-            transform.localPosition += new Vector3(pivotPos - prevPivot, 0f, 0f);
+            transform.localPosition += new Vector3(pivotPosX - prevPivotX, pivotPosY - prevPivotY, 0f);
             // update old pivot positon
-            prevPivot = pivotPos;
+            prevPivotX = pivotPosX;
+            prevPivotY = pivotPosY;
             timer = 0.3f;
         }
         else
