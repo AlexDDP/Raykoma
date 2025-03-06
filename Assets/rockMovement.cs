763a2b141b1c029d1fb867524fcc28c67656b15a
oisinmaher <62963896+oisinmaher@users.noreply.github.com>
2025-03-06 23:04:43 +0000

    Update rockMovement.cs

--- Assets/rockMovement.cs
+++ Assets/rockMovement.cs
@@ -4,27 +4,28 @@ using TMPro;
 public class RockMovement : MonoBehaviour
 {
     public static float moveSpeed = 5f;  // Speed of the boat
+    public float speedMultiplier = 1.1f;
     private Rigidbody2D rb;
-    public float time10s = 10f;
-    public float timeTilBoost;
+    public float timer;
+    public float timeTilBoost = 5f;
 
 
     void Start()
     {
-        timeTilBoost = time10s;
+        timer = timeTilBoost;
         rb = GetComponent<Rigidbody2D>();  // Get Rigidbody2D component
     }
 
     void Update()
     {
-        if (timeTilBoost > 0)
+        if (timer > 0)
         {
-            timeTilBoost -= Time.deltaTime;
+            timer -= Time.deltaTime;
         }
-        if (timeTilBoost <= 0)
+        if (timer <= 0 && moveSpeed < 10f)
         {
-            timeTilBoost = time10s;
-            moveSpeed = (float)(moveSpeed * 1.5);
+            timer = timeTilBoost;
+            moveSpeed = moveSpeed * speedMultiplier;
         }
 
         Vector2 moveDirection = new Vector2(-1, 0).normalized;
