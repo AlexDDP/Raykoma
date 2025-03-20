c6f27e33075748852c21ad0b7acc1f81448f6012
Aryan Dhiman <aryanoisin.dhiman@gmail.com>
2025-03-20 15:56:13 +0000

    Update BoatCollision.cs

--- Assets/BoatCollision.cs
+++ Assets/BoatCollision.cs
@@ -7,47 +7,9 @@ using Unity.VisualScripting;
 public class BoatCollision : MonoBehaviour
 {
     private Rigidbody2D rb;
-<<<<<<< Updated upstream
-<<<<<<< HEAD
-<<<<<<< HEAD
-    public int lifeCount = 3;
-    public TMP_Text collisionText;
-    public TMP_Text Coins;
-    // initialises boatSprtie Change so i dont get stupid static error
-    private BoatSpriteChange boatSpriteChange;
-    public int playerCoins = 0;
-    
-    
-=======
     public static int lifeCount = 3;
     public GameObject effects;
 
->>>>>>> main
-=======
-<<<<<<< Updated upstream
-    public static int lifeCount = 3;
-    public GameObject effects;
-
-=======
-<<<<<<< Updated upstream
-    public static int lifeCount = 3;
-    public GameObject effects;
-
->>>>>>> Stashed changes
-=======
-    public int lifeCount = 3;
-    public TMP_Text collisionText;
-    public TMP_Text Coins;
-    // initialises boatSprtie Change so i dont get stupid static error
-    private BoatSpriteChange boatSpriteChange;
-    public int playerCoins = 0;
-    
-    
->>>>>>> Stashed changes
-<<<<<<< Updated upstream
->>>>>>> main
-=======
->>>>>>> Stashed changes
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -71,21 +33,6 @@ public class BoatCollision : MonoBehaviour
             }
             Destroy(collision.gameObject);
         }
-<<<<<<< Updated upstream
-<<<<<<< HEAD
-<<<<<<< HEAD
-        if(collision.gameObject.CompareTag("Coin")) 
-        {
-            playerCoins++;
-            Coins.text = "Coins: " + playerCoins;
-        }
-=======
-=======
-<<<<<<< Updated upstream
->>>>>>> main
-=======
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
         GameObject effectInstance = Instantiate(effects, transform.position, Quaternion.identity);
 
         // Get the ParticleSystem component and play it
@@ -97,23 +44,6 @@ public class BoatCollision : MonoBehaviour
 
         // Destroy the effect after it's done playing
         Destroy(effectInstance, ps.main.duration);
-<<<<<<< Updated upstream
-<<<<<<< HEAD
->>>>>>> main
-=======
-=======
->>>>>>> Stashed changes
-=======
-        if(collision.gameObject.CompareTag("Coin")) 
-        {
-            playerCoins++;
-            Coins.text = "Coins: " + playerCoins;
-        }
->>>>>>> Stashed changes
-<<<<<<< Updated upstream
->>>>>>> main
-=======
->>>>>>> Stashed changes
         //marwans line of code(uknknown purpose but it works)
         rb.angularVelocity = 0f;
         }
