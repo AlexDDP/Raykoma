b5026ddfa42c510ee45f938809eaa21af9502e46
marwansabry1 <marwansabri@hotmail.com>
2025-03-20 16:05:39 +0000

    coins + rock sound

--- Assets/BoatCollision.cs
+++ Assets/BoatCollision.cs
@@ -3,22 +3,33 @@ using TMPro;
 using System;
 using System.IO;
 using Unity.VisualScripting;
+using UnityEngine.Audio;
 
 public class BoatCollision : MonoBehaviour
 {
     private Rigidbody2D rb;
     public static int lifeCount = 3;
     public GameObject effects;
+    public AudioClip rockCollisionSound;
+    // Reference to the rock collision sound clip
+    private AudioSource audioSource;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     void OnCollisionEnter2D(Collision2D collision)
     {
+        
         if (collision.gameObject.CompareTag("Rock") || collision.gameObject.CompareTag("Crocodile"))
         {
+            if (collision.gameObject.CompareTag("Rock"))
+            {
+                audioSource.PlayOneShot(rockCollisionSound);
+            }
+            
             lifeCount--;
             if (lifeCount <= 0)
             {
--- Assets/coinAccum.cs
+++ Assets/coinAccum.cs
@@ -1,22 +1,45 @@
 using UnityEngine;
 using TMPro;
-using System;
 
 public class coinAccum : MonoBehaviour
 {
     public int coins = 0;
     public TMP_Text coinsText;
+    public AudioClip coinSound;
+    private AudioSource audioSource;
+
+    void Start()
+    {
+        
+        audioSource = GetComponent<AudioSource>();
+
+        if (audioSource == null)
+        {
+            Debug.LogWarning("AudioSource is missing on the coin GameObject!");
+        }
+    }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
+        
         if (other.gameObject.CompareTag("Coins"))
         {
+          
             coins++;
             coinsText.text = "Coins: " + coins;
 
+          
+            if (audioSource != null && coinSound != null)
+            {
+                audioSource.PlayOneShot(coinSound); 
+            }
+            else
+            {
+                Debug.LogWarning("audioSource or coinSound is missing.");
+            }
+
+           
             Destroy(other.gameObject);
         }
     }
 }
-
-
