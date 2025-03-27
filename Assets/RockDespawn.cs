9e974e4d0adeb1cdada2e6429db7de127ccb7069
Aryan Dhiman <aryanoisin.dhiman@gmail.com>
2025-03-27 15:50:54 +0000

    Revert "Acheivement"
    This reverts commit 2c83f77fb17f22831ab1ef6a7d4ea8f727474d38.

--- Assets/RockDespawn.cs
+++ Assets/RockDespawn.cs
@@ -5,8 +5,6 @@ public class RockDespawn : MonoBehaviour
     public GameObject boat;  // Reference to the boat
     public float despawnDistance = 30f;  // Distance at which rocks despawn
 
-    public int RocksDodged = 0; // Number of rocks dodged
-
     void Update()
     {
         if (boat != null)
@@ -15,13 +13,6 @@ public class RockDespawn : MonoBehaviour
             if (Mathf.Abs(transform.position.x - boat.transform.position.x) > despawnDistance)
             {
                 Destroy(gameObject);  // Destroy the rock object
-                RocksDodged++;  // Increment the number of rocks dodged
-
-                if(RocksDodged > 20) 
-                {
-                    AchievementManager.Instance.UnlockAchievement("Rock Dodger 1!!");  // Unlock the "Dodged 20 Rocks" achievement
-
-                }
             }
         }
     }
