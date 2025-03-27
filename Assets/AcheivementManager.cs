2c83f77fb17f22831ab1ef6a7d4ea8f727474d38
Aryan Dhiman <aryanoisin.dhiman@gmail.com>
2025-03-27 15:46:33 +0000

    Acheivement

---
+++ Assets/AcheivementManager.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AchievementManager : MonoBehaviour
+{
+    public static AchievementManager Instance;
+
+    [Serializable]
+    public class Achievement
+    {
+        public string name;
+        public string description;
+        public bool unlocked;
+    }
+
+    public List<Achievement> achievements = new List<Achievement>();
+
+    private void Awake()
+    {
+        if (Instance == null)
+            Instance = this;
+        else
+            Destroy(gameObject);
+    }
+
+    public void UnlockAchievement(string achievementName)
+    {
+        Achievement achievement = achievements.Find(a => a.name == achievementName);
+        if (achievement != null && !achievement.unlocked)
+        {
+            achievement.unlocked = true;
+            Debug.Log($"Achievement Unlocked: {achievementName}");
+            
+        }
+    }
+}
+
+
