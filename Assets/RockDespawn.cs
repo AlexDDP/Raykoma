33b461ba0d5b8fb103ddc5b8928cac5901e63b81
Aryan Dhiman <aryanoisin.dhiman@gmail.com>
2025-04-03 16:08:47 +0100

    s

--- Assets/RockDespawn.cs
+++ Assets/RockDespawn.cs
@@ -2,9 +2,8 @@ using UnityEngine;
 
 public class RockDespawn : MonoBehaviour
 {
-   public GameObject boat;  // Reference to the boat
-   public float despawnDistance = 30f;  // Distance at which rocks despawn
-   public int RocksDodged = 0; // Number of rocks dodged
+    public GameObject boat;  // Reference to the boat
+    public float despawnDistance = 30f;  // Distance at which rocks despawn
 
     public int rocksDodged = 0; // Number of rocks dodged
 
@@ -13,6 +12,42 @@ public class RockDespawn : MonoBehaviour
 
 
     void Update()
+    {
+        if (boat != null)
+        {
+            // Check if the rock is beyond a certain distance from the boat
+            if (Mathf.Abs(transform.position.x - boat.transform.position.x) > despawnDistance)
+            {
+                Destroy(gameObject);  // Destroy the rock object
+                rocksDodged++;  // Increment the number of rocks dodged
+                if(rocksDodged > 20)
+                {
+                    AchievementManager.Instance.UnlockAchievement("Rock dodger 1!!!"); // Unlock the achievement
+                    Debug.Log("Achievement Unlocked: Rock dodger 1!!!");
+                    coinAccum.coins += 50; // Add 50 coins to the player's total
+                } else if(rocksDodged > 50)
+                {
+                    AchievementManager.Instance.UnlockAchievement("Rock dodger 2!!!"); // Unlock the achievement
+                    Debug.Log("Achievement Unlocked: Rock dodger 2!!!");
+                    coinAccum.coins += 100; // Add 100 coins to the player's total
+                } else if(rocksDodged > 100)
+                {
+                    AchievementManager.Instance.UnlockAchievement("Rock dodger 3!!!"); // Unlock the achievement
+                    Debug.Log("Achievement Unlocked: Rock dodger 3!!!");
+                    coinAccum.coins += 150; // Add 200 coins to the player's total
+                }
+            }
+        }
+}
+}
+using UnityEngine;
+
+public class RockDespawn : MonoBehaviour
+{
+   public GameObject boat;  // Reference to the boat
+   public float despawnDistance = 30f;  // Distance at which rocks despawn
+   public int RocksDodged = 0; // Number of rocks dodged
+
    void Update()
     {
         if (boat != null)
