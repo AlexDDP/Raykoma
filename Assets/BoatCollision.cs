70931ae4c80517366db0116048dff862b1d5bdaa
oisinmaher <62963896+oisinmaher@users.noreply.github.com>
2025-04-10 15:59:24 +0100

    Finish Line fix

--- Assets/BoatCollision.cs
+++ Assets/BoatCollision.cs
@@ -4,6 +4,7 @@ using System;
 using System.IO;
 using Unity.VisualScripting;
 using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
 
 public class BoatCollision : MonoBehaviour
 {
@@ -27,27 +28,28 @@ public class BoatCollision : MonoBehaviour
     {
         if (collision.gameObject.CompareTag("FinishLine"))
         {
-            audioSource.PlayOneShot(gameOverSound);
-            ScoreUpdate.gameEnded = true;
-            WaterDrag.terminate = true;
-            RockSpawner.spawnRocks = false;
-            GameProperties.objectMoveSpeed = 0f;
-            BoatController.moveSpeed = 0f;
-            ScrollingBackground.scrollSpeed = 0f;
-            coinSpawner.spawnCoins = false;
-            CrocodileSpawner.spawnCrocodiles = false;
-            LogSpawn.spawnLogs = false;
-            if (backgroundMusic != null)
-            {
-                backgroundMusic.Stop();  // Stop the background music from playing
-            }
+            SceneManager.LoadSceneAsync("Cycling");
+            GameProperties.healthPoints = 3;
+            //audioSource.PlayOneShot(gameOverSound);
+            //ScoreUpdate.gameEnded = true;
+            //WaterDrag.terminate = true;
+            //RockSpawner.spawnRocks = false;
+            //GameProperties.objectMoveSpeed = 0f;
+            //BoatController.moveSpeed = 0f;
+            //ScrollingBackground.scrollSpeed = 0f;
+            //coinSpawner.spawnCoins = false;
+            //CrocodileSpawner.spawnCrocodiles = false;
+            //LogSpawn.spawnLogs = false;
+            //if (backgroundMusic != null)
+            //{
+            //    backgroundMusic.Stop();  // Stop the background music from playing
+            //}
             Destroy(collision.gameObject);
 
         }
         if (collision.gameObject.CompareTag("Rock") || collision.gameObject.CompareTag("Crocodile"))
         {
             GameProperties.healthPoints--;
-            if (GameProperties.healthPoints <= 0)
             if (collision.gameObject.CompareTag("Rock"))
             {
                 audioSource.PlayOneShot(rockCollisionSound);
