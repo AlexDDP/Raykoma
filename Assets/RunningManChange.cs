115043a772ec7bc357204958c32ff4f4d01ecb55
oisinmaher <62963896+oisinmaher@users.noreply.github.com>
2025-04-16 23:06:26 +0100

    running animation

--- Assets/RunningManChange.cs
+++ Assets/RunningManChange.cs
@@ -2,19 +2,30 @@ using UnityEngine;
 
 public class RunningManChange : MonoBehaviour
 {
-    public Sprite[] heartSprites = new Sprite[4]; 
+    public Sprite[] characterPositions = new Sprite[4]; 
     private SpriteRenderer spriteRenderer;
-    public static int life;
+    public float iterationTime;
+    public float timer;
+    public static int index;
 
     void Start()
     {
-        life = GameProperties.healthPoints;
+        index = 0;
+        iterationTime = 0.2f;
+        timer = iterationTime;
         spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     public void Update()
     {
-        life = Mathf.Max(GameProperties.healthPoints, 0);
-        spriteRenderer.sprite = heartSprites[life];
+        if(timer < 0)
+        {
+            index++;
+            if (index == 4) index = 0;
+            spriteRenderer.sprite = characterPositions[index];
+            timer = iterationTime;
+        }
+        timer -= Time.deltaTime;
+        
     }
 }
